04143b48f87a813b6dd124ec5c1d9fe5caad49d1
Rabuno <kichsatst123@gmail.com>
2026-04-20 19:11:10 +0700

    Implement initial entity models and DbContext for review service

--- UGem.Api/Program.cs
+++ UGem.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+using MediaService = UGem.Service.MediaService;
+using CloudinaryService = UGem.Service.CloudinaryService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<MediaService.IService, CloudinaryService.Service>();
 
 var app = builder.Build();
 
--- UGem.Repositories/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace UGem.Repositories;
-
-public class Class1
-{
-}
