9a18ce03b0cb330c10ff1fda00123659e51d93f8
Rabuno <kichsatst123@gmail.com>
2026-04-29 22:54:26 +0700

    feat: integrate NetTopologySuite with PostgreSQL for enhanced spatial data support

--- UGem.Api/Program.cs
+++ UGem.Api/Program.cs
@@ -26,7 +26,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        o => o.UseNetTopologySuite()
     )
 );
 builder.Services.AddJwtServices(builder.Configuration);
