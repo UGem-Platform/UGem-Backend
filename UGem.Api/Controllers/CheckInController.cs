09d7780af9522aa49fbd50d4d5cd1d95796f6a03
dongquochuy171@gmail.com <dongquochuy171@gmail.com>
2026-05-03 10:57:52 +0700

    feat: fix qr

--- UGem.Api/Controllers/CheckInController.cs
+++ UGem.Api/Controllers/CheckInController.cs
@@ -1,6 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UGem.Api.Extensions;
+using UGem.Services.CheckInService;
+
 namespace UGem.Api.Controllers;
 
-public class CheckInController
+public class CheckInController : ControllerBase
 {
+    private readonly IService _service;
+
+    public CheckInController(IService service)
+    {
+        _service = service;
+    }
     
+    [Authorize(Policy = JwtExtensions.MerchantPolicy)]
+    [HttpGet("generate-qr/{orderId}")]
+    public IActionResult GenerateQrCode(Guid orderId)
+    {
+        var qrText = "https://www.youtube.com/watch?v=XWt96eZphlU";
+        var qrCodeBytes = _service.GenerateQrCode(qrText);
+        return File(qrCodeBytes, "image/png");
+    }
 }
--- UGem.Services/CheckInService/Service.cs
+++ UGem.Services/CheckInService/Service.cs
@@ -1,6 +1,14 @@
+using QRCoder;
+
 namespace UGem.Services.CheckInService;
 
 public class Service
 {
-    
+    public byte[] GenerateQrCode(string text)
+    {
+        using var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+        var qrCode = new PngByteQRCode(qrCodeData);
+        return qrCode.GetGraphic(20);
+    }
 }
