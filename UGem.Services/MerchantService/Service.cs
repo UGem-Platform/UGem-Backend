39a4be49ebf0c86aa2dcb6d1f23f4c8cd50b9fec
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-05-08 02:08:12 +0700

    add merchant rebalancing and underrated ranking and search orderbyDescending underrateScore

--- UGem.Services/MerchantService/Service.cs
+++ UGem.Services/MerchantService/Service.cs
@@ -71,7 +71,8 @@ public class Service : IService
         var totalItems = await queryByDistance.CountAsync();
 
         var queryOrder = queryByDistance
-            .OrderBy(m => m.Location.Distance(userLocation));
+            .OrderByDescending(m => m.UnderratedScore)
+            .ThenBy(m => m.Location.Distance(userLocation));
 
         var query = queryOrder
             .Skip((request.PageIndex - 1) * request.PageSize)
